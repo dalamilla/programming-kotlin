acd3c07d5847974eeeb5c700ca425415d7914bff
Daniel Alamilla <daniel.alamilla.co@gmail.com>
2025-05-23 18:15:12 -0600

    add tip calculator for android jetpack compose

--- general/android-compose/README.md
+++ general/android-compose/README.md
@@ -7,3 +7,4 @@ List of Android Jetpack Compose projects.
 |  #  | Name                                                | Description           |
 | ----| ----------------------------------------------------| ----------------------|
 |  01 | [Dice Roller](./dice-roller/README.md)              | Dice Roller App.      |
+|  02 | [Tip Calculator](./tip-calculator/README.md)        | Tip Calculator App.   |
---
+++ general/android-compose/tip-calculator/app/src/main/java/com/dalamilla/tipcalculator/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.dalamilla.tipcalculator
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.dalamilla.tipcalculator.ui.theme.TipCalculatorTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
+        super.onCreate(savedInstanceState)
+        setContent {
+            TipCalculatorTheme {
+                Surface(modifier = Modifier.fillMaxSize()) { TipCalculatorLayout() }
+            }
+        }
+    }
+}
